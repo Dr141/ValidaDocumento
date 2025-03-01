8bb583115f670bcc18eadca228a94f345aab1abb
Rhemerson.monteiro <rhemerson.monteiro@engegraph.com.br>
2025-03-01 11:41:44 -0300

    SavePoint: implementador removedor de formatação e testes unitarios.

--- Documentos/Extensoes/Formatar.cs
+++ Documentos/Extensoes/Formatar.cs
@@ -10,8 +10,8 @@ public static class Formatar
     /// <summary>
     /// Método para formata CPF.
     /// </summary>
-    /// <param name="cpf"></param>
-    /// <returns>Rertona uma <see cref="string"/> com o número do CPF formatado.</returns>
+    /// <param name="cpf">deverá passar o número do CPF no formato <see cref="string"/></param>
+    /// <returns>Retorna uma <see cref="string"/> com o número do CPF formatado.</returns>
     /// <exception cref="ValidationException">Rertona uma exception com os erros encontrado no documento.</exception>
     public static string FormatarCPF(this string cpf)
     {
@@ -22,12 +22,30 @@ public static class Formatar
     /// <summary>
     /// Método para formata CNPJ.
     /// </summary>
-    /// <param name="cnpJ"></param>
-    /// <returns>Rertona uma <see cref="string"/> com o número do CNPJ formatado.</returns>
+    /// <param name="cnpj">deverá passar o número do CNPJ no formato <see cref="string"/></param>
+    /// <returns>Retorna uma <see cref="string"/> com o número do CNPJ formatado.</returns>
     /// <exception cref="ValidationException">Rertona uma exception com os erros encontrado no documento.</exception>
     public static string FormatarCNPJ(this string cnpj)
     {
         cnpj.ValidaCnpjSemFormatacao();
         return Convert.ToUInt64(cnpj).ToString("00\\.000\\.000\\/0000\\-00");
+    }
+
+    /// <summary>
+    /// Método para remover formatação do CPF/CNPJ.
+    /// </summary>
+    /// <param name="documento">deverá passar o número do CPF/CNPJ no formato <see cref="string"/></param>
+    /// <returns>retorna uma <see cref="string"/> com o número do CPF/CNPJ.</returns>
+    public static string RemoverFormatacao(this string documento)
+    {
+        string apenasNumeros = string.Empty;
+
+        for (int i = 0; i < documento.Length; i++)
+        {
+            if(char.IsDigit(documento[i])) 
+                apenasNumeros += documento[i];
+        }
+
+        return apenasNumeros;
     }
 }
